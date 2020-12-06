a8f47b19b4672d99316bd699641c2914deb24c04
Mikail Gedik <mikail.gedik@bluewin.ch>
2020-12-06 17:04:45 +0100

    Revert "Modules"
    This reverts commit 9e24255fb69bc4db25cf3ecba8b06f9f88e93010.

--- src/main/java/com/rayx/RayX.java
+++ src/main/java/com/rayx/RayX.java
@@ -5,8 +5,8 @@ import com.rayx.opencl.OpenCLHelper;
 import static org.lwjgl.opencl.CL22.*;
 
 public class RayX {
+
     public static void main(String[] args) {
-        System.out.println();
         long device = OpenCLHelper.getAllAvailableDevices().get(0);
         System.out.println("CL: Name: " + OpenCLHelper.queryDeviceInfo(device, CL_DEVICE_NAME));
     }
